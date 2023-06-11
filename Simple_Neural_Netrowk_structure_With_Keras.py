ea379637a3e1d5a7c1f241af9ca6f70270ac1a96
Saleheen <61604951+Ashraf-Saleheen@users.noreply.github.com>
2023-06-11 11:05:59 +0600

    Update Simple_Neural_Netrowk_structure_With_Keras.py
    spelling mistake fixed
    and order mistake fixed

--- Simple_Neural_Netrowk_structure_With_Keras.py
+++ Simple_Neural_Netrowk_structure_With_Keras.py
@@ -1,69 +1,34 @@
-
-import tensorflow
+import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Activaotion, Maxpooling2D
-from tensorflow.keras.datasets import fashion_mnist
+from keras.datasets import mnist
+from keras.layers import Conv2D, Dense, Activation, MaxPooling2D, Flatten
+from keras.models import Sequential
 
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load()
+fashion_data = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = fashion_data.load_data()
 
 x_train = x_train.reshape(-1, 28, 28, 1)
-y_test = y_test.reshape(-1, 28, 28, 1)
+x_test = x_test.reshape(-1, 28, 28, 1)
 
 def build_model():
-    model = keras.model.sequential()
+    model = Sequential()
 
-    model.add(Conv2D(32, (3,3), input_shape= x_train.shape[1:]))
-    model.add(Activaotion("relu"))
-    model.add(Maxpooling2D(pool_size=(2,2)))
+    model.add(Conv2D(32, (3, 3), input_shape=x_train.shape[1:]))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32,(3,3)))
-    model.add(Activaotion("relu"))
-    model.add(Maxpooling2D(pool_size=(2,2)))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Dense)
-    model.add(Flatten(10))
-    model.add(Activaotion("softmax"))
+    model.add(Flatten())
+    model.add(Dense(10))
+    model.add(Activation("softmax"))
 
     model.compile(optimizer="adam",
-                  loss = "sparse_Categorical_Crossentropy",
-                   metrics= "[accuracy]" )
+                  loss="sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
     return model
+
 model = build_model()
-model.fit([x_train], [y_train], batch_size=64, epoch = 5, validation_data=(x_test, y_test))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+model.fit(x_train, y_train, batch_size=64, epochs=5, validation_data=(x_test, y_test))
